60b9c73939f397ab1a47b202d4c2a89d6d20da92
DataSperling <simon.francis.mrsc@tutanota.de>
2022-03-07 16:33:14 +0100

    added maven project with JPA PostgreSQL and Spring WEB

---
+++ src/main/java/com/datasperling/SQLPatientSystem/SqlPatientSystemApplication.java
@@ -0,0 +1,13 @@
+package com.datasperling.SQLPatientSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SqlPatientSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SqlPatientSystemApplication.class, args);
+	}
+
+}
