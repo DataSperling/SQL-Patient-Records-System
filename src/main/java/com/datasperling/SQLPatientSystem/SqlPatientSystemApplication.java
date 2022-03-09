42954b6c2de48c4da67ef301e24e4b6c1885368b
DataSperling <simon.francis.mrsc@tutanota.de>
2022-03-09 13:42:44 +0100

    modified GetMapping request to use correct patient object

--- src/main/java/com/datasperling/SQLPatientSystem/SqlPatientSystemApplication.java
+++ src/main/java/com/datasperling/SQLPatientSystem/SqlPatientSystemApplication.java
@@ -1,10 +1,13 @@
 package com.datasperling.SQLPatientSystem;
 
+import com.datasperling.SQLPatientSystem.patient.Patient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDate;
+import java.time.Month;
 import java.util.List;
 
 @SpringBootApplication
@@ -17,8 +20,21 @@ public class SqlPatientSystemApplication {
 	}
 
 	@GetMapping
-	public List<String> hello() {
-		return List.of("Hello",  "World");
+	public List<Patient> hello() {
+		return List.of(
+				new Patient(
+						1L,
+						"Mariam",
+						null,
+						"Smith",
+						"FEMALE",
+						LocalDate.of(2000, Month.JANUARY, 5),
+						"15 winchester av",
+						null,
+						null,
+						false
+				)
+		);
 	}
 
 }
